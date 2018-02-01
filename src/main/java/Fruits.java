337ccb2bb478d8088ebfd25c6d7182011b05aef1
kuirons <wuguangyu@popcornie.com>
2018-02-01 14:25:01 +0800

    finish:Fruits

---
+++ src/main/java/Fruits.java
@@ -0,0 +1,107 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author WuGYu
+ * @date 2018/2/1 11:37
+ */
+public class Fruits {
+  // 决策
+  public static <T> List<T> predicateFruit(List<T> fruits, FruitsPredicate<T> predicate) {
+    List<T> resultList = new ArrayList<>();
+    for (T t : fruits) {
+      if (predicate.test(t)) resultList.add(t);
+    }
+    return resultList;
+  }
+
+  public static void main(String[] args) {
+    List result = new ArrayList();
+    List<Apple> appleList = new ArrayList<>();
+    appleList.add(new Apple("red", 200));
+    appleList.add(new Apple("green", 600));
+    // 判断苹果重量
+    result =
+        Fruits.predicateFruit(
+            appleList,
+            new FruitsPredicate<Apple>() {
+              @Override
+              public boolean test(Apple apple) {
+                return apple.getWeight() > 500 ? true : false;
+              }
+            });
+    for (Object a : result) {
+      System.out.println(((Apple) a).getWeight());
+    }
+    // 判断苹果颜色
+    result =
+        Fruits.predicateFruit(
+            appleList,
+            new FruitsPredicate<Apple>() {
+              @Override
+              public boolean test(Apple apple) {
+                return "red".equals(apple.getColor()) ? true : false;
+              }
+            });
+    for (Object a : result) {
+      System.out.println(((Apple) a).getColor());
+    }
+    // 判断香蕉重量......
+    // 判断香蕉颜色.....(都是套路~~~~)
+  }
+}
+
+interface FruitsPredicate<T> {
+  boolean test(T t);
+}
+
+class Apple {
+  private String color;
+  private int weight;
+
+  Apple(String color, int weight) {
+    this.color = color;
+    this.weight = weight;
+  }
+
+  Apple() {}
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  public String getColor() {
+
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+}
+
+class Banana {
+  private String color;
+  private int weight;
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+
+  public String getColor() {
+
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+}
